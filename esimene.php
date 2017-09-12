e12060deb81d7218f78dfe08df27024605bfc725
Anelle Avaste <gitanellea@users.noreply.github.com>
2017-09-12 14:15:37 +0300

    Lisatud on esimene jupp PHP koodi

---
+++ esimene.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+           <meta charset="utf-8">
+           <title>Anelle Avaste veebiprogrammeerimine</title>
+</head>
+<body>
+           <h1>Anelle Avaste veebiprogrammeerimine</h1>
+           <p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiselt võetavat sisu.</p>
+
+<body>
+	<h2> Minust </h2>
+	<p>Pärnust pärit infoteaduse kolmanda aasta tudeng.</p>
+	<p>Mulle meeldib suhelda erinevate inimestega ja olen väga suur kassisõber. Vanemate juures elab mul 3 armsat kiisut.</p>
+	<p>Olen lõbus, seltskondlik ja alati heatujuline inimene</p>
+
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud teksti!"; 
+	?>
+</body>	
+</html>
