26fec6b4d338767632ff4bd4e7c344ea41941174
Anelle Avaste <gitanellea@users.noreply.github.com>
2017-09-12 15:28:45 +0300

    Lisatud muutujad ja IF lause

--- esimene.php
+++ esimene.php
@@ -1,3 +1,31 @@
+<?php
+	//see on kommentaar, järgmisena paar muutujat
+	$myName = "Anelle";
+	$myFamilyName = "Avaste";
+	//Vaatame, mis kell on ja määrame päeva osa
+	$hourNow = date("H");
+	//echo $hourNow;
+	$partOfDay = "";
+	if ($hourNow < 8){	//	<	>	<=	>=	!=	
+		$partOfDay = "varajane hommik";
+	}
+	if ($hourNow >= 8 and $hourNow <16){
+		$partOfDay = "koolipäev";
+	}
+	if ($hourNow <16){
+		$partOfDay = "vabaaeg";
+	}
+	//echo $partOfDay;
+	
+	//vaatame, kaua on koolipäeva lõpuni aega jäänud
+	$timeNow = strtotime(date("d.m.Y H:i:s"));
+	//echo $timeNow;
+	$schoolDayEnd = strtotime(date("d.m.Y" ." " ."15:45"));
+	//echo $schoolDayEnd;
+	$toTheEnd = $schoolDayEnd - $timeNow;
+	echo $toTheEnd;
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,8 +33,12 @@
            <title>Anelle Avaste veebiprogrammeerimine</title>
 </head>
 <body>
-           <h1>Anelle Avaste veebiprogrammeerimine</h1>
-           <p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiselt võetavat sisu.</p>
+    <h1>
+	<?php
+		echo $myName ." ".$myFamilyName;
+	?>
+	veebiprogrammeerimine</h1>
+    <p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiselt võetavat sisu.</p>
 
 <body>
 	<h2> Minust </h2>
@@ -15,7 +47,10 @@
 	<p>Olen lõbus, seltskondlik ja alati heatujuline inimene</p>
 
 	<?php
-		echo "See on esimene jupp PHP abil väljastatud teksti!"; 
+		echo "<p>See on esimene jupp PHP abil väljastatud teksti!</p>"; 
+		echo "<p>Täna on ";
+		echo date("d.m.Y") .", kell lehe avamisel oli" . date("H:i:s");
+		echo ", käes on " . $partOfDay.".</p>";
 	?>
 </body>	
 </html>
