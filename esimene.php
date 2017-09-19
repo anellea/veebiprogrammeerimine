fe4c076351e5fcd17bf2f6ed95c19800cda31776
Anelle Avaste <gitanellea@users.noreply.github.com>
2017-09-19 14:28:58 +0300

    Lisatud on 3. loengu materjal

--- esimene.php
+++ esimene.php
@@ -2,6 +2,12 @@
 	//see on kommentaar, järgmisena paar muutujat
 	$myName = "Anelle";
 	$myFamilyName = "Avaste";
+	
+	$monthNamesEt = ["jaanuar", "veebruar", "märts", "aprill", " mai", "juuni", "juuli", "august", " september", "oktoober", "november", "detsember"];
+	//var_dump($monthNamesEt);
+	//echo $monthNamesEt[1];
+	$monthNow = $monthNamesEt[date("n")-1];
+	
 	//Vaatame, mis kell on ja määrame päeva osa
 	$hourNow = date("H");
 	//echo $hourNow;
@@ -23,7 +29,33 @@
 	$schoolDayEnd = strtotime(date("d.m.Y" ." " ."15:45"));
 	//echo $schoolDayEnd;
 	$toTheEnd = $schoolDayEnd - $timeNow;
-	echo $toTheEnd;
+	//echo (round($toTheEnd / 60));
+	
+	//tegeleme vanusega
+	$myBirthYear;
+	$ageNotice = "";
+	//var_dump($_POST");
+	if (isset($_POST["birthYear"]) ){
+		$myBirthYear = $_POST["birthYear"];
+		$myAge = date("Y") - $_POST["birthYear"];
+		// echo $myAge;
+		$ageNotice = "<p>Teie vanus on ligikaudu " . $myAge ." aastat.</p>";
+		
+		$ageNotice .= "<p>Olete elanud järgnevatel aastatel:</p>";
+		$ageNotice .= "<ul>";
+		
+		$yearNow = date("Y");
+		for ($i = $myBirthYear; $i <= $yearNow; $i ++) {
+			$ageNotice .= "<li>" .$i ."</li> \n";
+		}
+		$ageNotice .= "</ul>";
+	}
+	
+	//teeme tsükli
+	/*for ($i = 0; $i < 5; $i ++){
+		echo "ha";
+	}*/
+	
 ?>
 
 <!DOCTYPE html>
@@ -49,8 +81,43 @@
 	<?php
 		echo "<p>See on esimene jupp PHP abil väljastatud teksti!</p>"; 
 		echo "<p>Täna on ";
-		echo date("d.m.Y") .", kell lehe avamisel oli" . date("H:i:s");
+		echo date("d. ") . $monthNow .date(" Y") .", kell lehe avamisel oli" . date("H:i:s");
 		echo ", käes on " . $partOfDay.".</p>";
 	?>
+	
+	<h2>Natuke aastaarvudest</h2>
+	<form method="POST">
+		<label>Teie sünniaasta: </label>
+		<input type="number" name="birthYear" min="1900" max="2017" value="<?php 
+		echo $myBirthYear; ?>">
+		<input type="submit" name="submitBirthYear" value="Kinnita">
+	
+	</form>
+	<?php
+		if ($ageNotice != "") {
+			echo $ageNotice;
+		}
+	?>
+	
 </body>	
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
