44414dbb463a12113fc8041e0097289eca8cf29d
Anelle Avaste <gitanellea@users.noreply.github.com>
2017-09-26 14:06:07 +0300

    Lisatud on sünnikuupäev

--- tund3/foto.php
+++ tund3/foto.php
@@ -1,12 +1,19 @@
 <?php
 	$picsDir= "../../pildid/";
+	$picFileTypes = ["jpg", "jpeg", "png", "gif"];
+	$picFiles = []; //massiiv
 	
-	$picFiles = [];
-	
-	$allFiles = scandir($picsDir);
+	$allFiles = array_slice(scandir($picsDir), 2); 					//vahepalaks kus asjad picsdir-iga sisse loetud (andis rohkem kui tahtsime)
 	//var_dump ($allFiles);
+	foreach ($allFiles as $file){
+		$fileType =  pathinfo ($file, PATHINFO_EXTENSION);
+		//kontrollime
+		if (in_array($fileType, $picFileTypes) == true) {
+			array_push($picFiles, $file);
+		}
+	}
 	
-	$picFiles = array_slice($allFiles, 2);
+	//$picFiles = array_slice($allFiles, 2); 				//array_slicega viskasime ära kaks punkti
 	//var_dump ($picFiles);
 	
 	$picCount = count($picFiles);
@@ -24,7 +31,7 @@
 <body>
     <h1>Foto</h1>
     <p>See leht on loodud õppetöö raames ning ei sisalda mingit tõsiselt võetavat sisu.</p>
-	<img src="<?php echo $picsDir .$picFile; ?>" alt="Tallinna Ülikool" >
+	<img src="<?php echo $picsDir .$picFile; ?>" alt="Tallinna Ülikool" > //
 
 </body>	
 </html>
